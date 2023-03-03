2a14e7dcdb9785004e65e82308622ec0dedbfec2
Efrain May Mayo <efrain_may_75@hotmail.com>
2023-03-03 00:28:51 -0600

    post + getbyid pokemon

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,15 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    //is int, isPOsitive, min 1
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+    //isString, 
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+}
