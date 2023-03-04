1abda1f625ec74e9f819de51bb5a0b77a51bc78e
Efrain May Mayo <efrain_may_75@hotmail.com>
2023-03-04 12:40:19 -0600

    configurationService

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -6,14 +6,22 @@ import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService,
+  ){
+
+    this.defaultLimit = configService.get<number>('defaultLimit');
+    
+  }
   
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -35,7 +43,7 @@ export class PokemonService {
 
   findAll(paginationDto: PaginationDto) {
 
-    const { limit =  10, offset = 0 } = paginationDto; 
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto; 
 
     return this.pokemonModel.find()
       .limit(limit)
