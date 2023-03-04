6dcda7c9530b619a58458239560ac5ace1d71462
Efrain May Mayo <efrain_may_75@hotmail.com>
2023-03-04 10:48:24 -0600

    evitar el tranform options

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -13,3 +13,47 @@ export class PaginationDto {
     @IsPositive()
     offset?: number;
 }
+
+/* Si alguno de ustedes NO quiere aplicar esta regla al main, ya sea por performance u otro motivo, les recomiendo "declarar el tipo en la clase PaginationDto" */
+
+/*
+    export class PaginationDto {
+    
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    @Min(1)
+    @Type(() => Number)
+    limit?: number;
+ 
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    @Type(() => Number)
+    offset?: number;
+
+    Con esto evitamos la delación global en el main y mantenemos un control nosotros.
+}
+*/
+
+// OTRA FORMA 
+
+/*
+export class PaginationDto {
+    
+    @Transform(({ value }) => toNumber(value)) 
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    @Min(1)
+    limit?: number;
+ 
+    @Transform(({ value }) => toNumber(value)) 
+    @IsOptional()
+    @IsPositive()
+    @IsNumber()
+    offset?: number;
+
+    Lo siguiente que se hace es poner un decorator llamado @Transform en el fichero DTO, el cual recibe una callback y que va a llamar la funcion toNumber() creada anteriormente
+}
+*/
---
+++ src/common/helpers/ValueTransform.helper.ts
@@ -0,0 +1,5 @@
+export const toNumber = (value: string): number => {
+    const newValue: number = Number.parseInt(value);
+
+    return newValue;
+}
